9962021fa9b74db59241ba10c2254175e8041186
kris.he <kris.he@ebestmobile.com>
2016-11-09 11:39:00 +0800

    添加项目文件。

---
+++ app.js
@@ -0,0 +1,61 @@
+﻿/**************************导入需要的包************************************************/
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+/**************************设置nodejs路由对应的文件***************************/
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var product = require('./routes/product');
+/**************************express配置模板视图**********************************/
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+/**************************引入要使用的模块**********************************************/
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+/**************************node路由配置**********************************/
+app.use('/', routes);
+app.use('/users', users);
+app.use('/product', product);
+/**************************捕获异常***********************************/
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ routes/product.js
@@ -0,0 +1,36 @@
+﻿var express = require('express');
+var router = express.Router();
+var config = require("config");
+var pg = require("pg");
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    //res.render('index', { title: 'Express' });
+
+    var sql = 'SELECT productcode as code,description as name,ebmobile__flavor__c as flavor,ebmobile__pack__c as package,ebmobile__brand__c as brand FROM sfdc5sqas.product2 LIMIT 100';
+
+    pg.defaults.ssl = true;
+    var client = new pg.Client(config.conString);
+
+    client.connect(function (err) {
+        if (err) throw err;
+
+        // execute a query on our database
+        client.query(sql, function (err, result) {
+            if (err) throw err;
+
+            // just print the result to the console
+            res.json(result.rows);
+            //console.log(result.rows[0]); // outputs: { name: 'brianc' }
+
+            // disconnect the client
+            client.end(function (err) {
+                if (err) throw err;
+            });
+        });
+    });
+
+});
+
+
+module.exports = router;
