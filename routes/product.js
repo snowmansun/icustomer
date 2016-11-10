3ac24cd7d90a43962ee4310715008976498bcd94
kris.he <kris.he@ebestmobile.com>
2016-11-10 18:29:37 +0800

    1

--- routes/product.js
+++ routes/product.js
@@ -5,11 +5,27 @@ var db = require('../db/db');
 /* GET home page. */
 router.get('/', function (req, res) {
     //res.render('index', { title: 'Express' });
-    var sql = 'SELECT	productcode as code,description as name,ebmobile__flavor__c as flavor,ebmobile__pack__c as package,ebmobile__brand__c as brand FROM sfdc5sqas.product2 LIMIT 100';
+    var sql =
+        'SELECT' +
+        '   productcode AS code,' +
+        '   description AS NAME,' +
+        '   ebmobile__flavor__c AS flavor,' +
+        '   ebmobile__pack__c AS package,' +
+        '   ebmobile__brand__c AS brand,' +
+        '   uom.ebmobile__denominator__c as denominator,' +
+        '   0 as must_sku ' +
+        'FROM' +
+        '   sfdc5sqas.product2 p inner join sfdc5sqas.ebmobile__productuom__c uom ' +
+        '   on p.sfid = uom.ebmobile__productid__c ' +
+        'Where ebmobile__uomcode__c= \'EA\' and p.isactive = TRUE';
+
     db.query(sql)
         .then(function (result) {
             res.json(result.rows);
-    });
+        }).catch(function (err) {
+            console.error(err);
+        });
+
 
 });
 
--- routes/product_attr.js
+++ routes/product_attr.js
@@ -1,6 +1,6 @@
 ﻿var express = require('express');
-var router = express.Router();
 var db = require('../db/db');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
     var sql_flavor = 'SELECT DISTINCT ebmobile__flavor__c as value from sfdc5sqas.product2 where ebmobile__flavor__c is not NULL';
     var sql_pack = 'SELECT DISTINCT ebmobile__pack__c as value from sfdc5sqas.product2 where ebmobile__pack__c is not NULL';
 
-    var array_json = {
+    var res_json = {
         brand: new Array(),
         flavor: new Array(),
         pack: new Array()
@@ -17,26 +17,25 @@ router.get('/', function (req, res) {
     db.query(sql_brand)
         .then(function (result) {
             for (var i in result.rows) {
-                array_json.brand.push(result.rows[i].value);
+                res_json.brand.push(result.rows[i].value);
             }
 
             db.query(sql_flavor)
                 .then(function (result) {
                     for (var i in result.rows) {
-                        array_json.flavor.push(result.rows[i].value);
+                        res_json.flavor.push(result.rows[i].value);
                     }
 
                     db.query(sql_pack)
                         .then(function (result) {
                             for (var i in result.rows) {
-                                array_json.pack.push(result.rows[i].value);
+                                res_json.pack.push(result.rows[i].value);
                             }
 
-                            res.json(array_json);
+                            res.json(res_json);
                         });
                 });
         });
 });
-
 
 module.exports = router;
