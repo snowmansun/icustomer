f847a708fdbfc26f53b7c3b07a8c096eac2ba824
kris.he <kris.he@ebestmobile.com>
2016-11-10 15:55:08 +0800

    1

---
+++ routes/product_attr.js
@@ -0,0 +1,42 @@
+﻿var express = require('express');
+var router = express.Router();
+var db = require('../db/db');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    var sql_brand = 'SELECT DISTINCT ebmobile__brand__c as value from sfdc5sqas.product2 where ebmobile__brand__c is not NULL';
+    var sql_flavor = 'SELECT DISTINCT ebmobile__flavor__c as value from sfdc5sqas.product2 where ebmobile__flavor__c is not NULL';
+    var sql_pack = 'SELECT DISTINCT ebmobile__pack__c as value from sfdc5sqas.product2 where ebmobile__pack__c is not NULL';
+
+    var array_json = {
+        brand: new Array(),
+        flavor: new Array(),
+        pack: new Array()
+    }
+
+    db.query(sql_brand)
+        .then(function (result) {
+            for (var i in result.rows) {
+                array_json.brand.push(result.rows[i].value);
+            }
+
+            db.query(sql_flavor)
+                .then(function (result) {
+                    for (var i in result.rows) {
+                        array_json.flavor.push(result.rows[i].value);
+                    }
+
+                    db.query(sql_pack)
+                        .then(function (result) {
+                            for (var i in result.rows) {
+                                array_json.pack.push(result.rows[i].value);
+                            }
+
+                            res.json(array_json);
+                        });
+                });
+        });
+});
+
+
+module.exports = router;
